c4485f59297121a973faa7e7b3efc766a8b01340
Basit Shaikh <shaikhbasit279@gmail.com>
2024-12-14 21:02:21 +0500

    declaring all the variables

--- scripts.js
+++ scripts.js
@@ -1,34 +1,61 @@
 function renderTicTacToe() {
 
-  let html = `
+      let html = `
 
-  <h2>Tic Tac Toe</h2>
+      <h2>Tic Tac Toe</h2>
 
-  <div class="cell-container">
+      <div class="cell-container">
 
-    <div cellIndex="0" class="cell"></div>
-    <div cellIndex="1" class="cell"></div>
-    <div cellIndex="2" class="cell"></div>
-    <div cellIndex="3" class="cell"></div>
-    <div cellIndex="4" class="cell"></div>
-    <div cellIndex="5" class="cell"></div>
-    <div cellIndex="6" class="cell"></div>
-    <div cellIndex="7" class="cell"></div>
-    <div cellIndex="8" class="cell"></div>
+        <div cellIndex="0" class="cell"></div>
+        <div cellIndex="1" class="cell"></div>
+        <div cellIndex="2" class="cell"></div>
+        <div cellIndex="3" class="cell"></div>
+        <div cellIndex="4" class="cell"></div>
+        <div cellIndex="5" class="cell"></div>
+        <div cellIndex="6" class="cell"></div>
+        <div cellIndex="7" class="cell"></div>
+        <div cellIndex="8" class="cell"></div>
 
-  </div>
+      </div>
 
-  <div class="status-bar"></div>
+      <div class="status-bar"></div>
 
-  <div class="score-status"></div>
+      <div class="score-status"></div>
 
-  <button class="restart-button">Restart</button>
-  <button class="change-player js-change-player">Change Player</button>
+      <button class="restart-button">Restart</button>
+      <button class="change-player js-change-player">Change Player</button>
 
-`;
+    `;
 
-document.querySelector('.js-tic-tac-toe')
-.innerHTML = html;
+    document.querySelector('.js-tic-tac-toe')
+    .innerHTML = html;
+
+    const cells = document.querySelectorAll('.cell');
+    const statusText = document.querySelector('.status-bar');
+    const restartbutton = document.querySelector('.restart-button');
+    const scorestatus = document.querySelector('.score-status');
+    const changingplayer = document.querySelector('.js-change-player');
+
+    const winningCondition = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+
+    let options = ['', '', '', '', '', '', '', '', '',];
+    let currentPlayer = 'X';
+    let runningGame = false;
+
+    let score = {
+      X: 0,
+      O: 0,
+      Draw: 0,
+    }
 
 }
 
