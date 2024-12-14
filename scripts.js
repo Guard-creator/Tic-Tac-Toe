fa0043b7511b2d484601ebe5d7e69465ac34e9c5
Basit Shaikh <shaikhbasit279@gmail.com>
2024-12-14 21:05:22 +0500

    create all the functions and making tic tac toe interactive

--- scripts.js
+++ scripts.js
@@ -57,6 +57,129 @@ function renderTicTacToe() {
       Draw: 0,
     }
 
+    function startingGame() {
+
+      cells.forEach(cell => cell.addEventListener('click', cellClicked));
+      statusText.textContent = `${currentPlayer}'s turn`;
+      scorestatus.textContent = `X: ${score.X}, O: ${score.O} Draw: ${score.Draw}`;
+      restartbutton.addEventListener('click', restartbtn);
+      changingplayer.addEventListener('click', changePlayer)
+      runningGame = true;
+
+    }
+
+    function cellClicked() {
+
+      const cellIndex = this.getAttribute('cellIndex');
+
+      if(options[cellIndex] !== '' || !runningGame) {
+        return;
+      }
+
+      updateCell(this, cellIndex);
+      checkWinner();
+    
+      
+      if(runningGame && robotPlay === 'playWithRobot') {
+        setTimeout(robotMove, 500);
+      }
+      
+
+    }
+
+    function updateCell(cell, cellIndex) {
+
+      options[cellIndex] = currentPlayer;
+      cell.textContent = currentPlayer;
+
+    }
+
+    function changePlayer() {
+
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+      statusText.textContent = `${currentPlayer}'s turn`;
+
+    }
+
+    function checkWinner() {
+
+      let roundWon = false;
+
+      winningCondition.forEach(conditon => {
+
+        const cellA = options[conditon[0]];
+        const cellB = options[conditon[1]];
+        const cellC = options[conditon[2]];
+
+        if(cellA === '' || cellB === '' || cellC === '') {
+          return;
+        }
+
+        if(cellA === cellB && cellB === cellC) {
+          roundWon = true;
+          return;
+        } 
+
+      })
+
+      if(roundWon) {
+        statusText.textContent = `${currentPlayer}'s Wins!`;
+        updateScore('Win');
+        runningGame = false;
+      } else if (!options.includes('')) {
+        statusText.textContent = `Draw`;
+        updateScore('Draw');
+        runningGame = false;
+      } else {
+        changePlayer();
+      }
+
+    }
+
+    function restartbtn() {
+
+      options = ['', '', '', '', '', '', '', '', '',];
+      currentPlayer = 'X';
+      statusText.textContent = `${currentPlayer}'s turn`;
+      cells.forEach(cell => cell.textContent = '');
+      runningGame = true;
+
+    }
+
+    function robotMove() {
+      if (!runningGame) return;
+
+      const emptyCells = options
+        .map((val, index) => (val === '' ? index : null))
+        .filter(index => index !== null);
+
+      if (emptyCells.length === 0) return;
+
+      const randomIndex = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+      const cell = cells[randomIndex];
+
+      updateCell(cell, randomIndex);
+      checkWinner();
+    }
+
+    function updateScore(result) {
+
+      if(result === 'Win') {
+        
+        if(currentPlayer  === 'X') {
+          score.X++;
+        } else if (currentPlayer === 'O') {
+          score.O++;
+        }
+
+      } else if(result === 'Draw') {
+        score.Draw++;
+      }
+
+      scorestatus.textContent = `X: ${score.X}, O: ${score.O} Draw: ${score.Draw}`;
+    
+    }
+
 }
 
 document.querySelector('.js-play-friend')
